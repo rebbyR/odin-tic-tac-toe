bd49382b3393e02f2ed1b87b315a76d570f4c5cb
robby <5150reb@gmail.com>
2023-09-17 16:47:53 -0400

    display and object work, no win condition
    - added refreshDisplay()
    - added event listeners for each div
    - game functional besides logic to not override already chosen cells

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 
 const gameboard = (() => {
     const board = [
-        ['-', '-', '-'],
-        ['-', '-', '-'],
-        ['-', '-', '-']  
+        ['', '', ''],
+        ['', '', ''],
+        ['', '', '']  
     ];
 
     return {board};
@@ -44,12 +44,13 @@ const game = ((playerOneName = "Player One", playerTwoName = "Player Two") => {
 
 })();
 
-
 //worry about this after things work in console
 const displayController = (() => {
     //display stuff here
 
     const displayBoard = () => {
+        //create empty display before refreshing
+
         for (i in gameboard.board) {
             let row = document.body.appendChild(document.createElement('div'));
             row.classList.add(`row`);
@@ -60,27 +61,113 @@ const displayController = (() => {
 
         for (i in gameboard.board[0]) {
             let cell = document.createElement('div');
+            cell.textContent = gameboard.board[0][i];
             cell.classList.add('cell');
             cell.setAttribute('id', `0-${i}`);
             rows[0].appendChild(cell);
         };
         for (i in gameboard.board[1]) {
             let cell = document.createElement('div');
+            cell.textContent = gameboard.board[1][i];
+
             cell.classList.add('cell');
             cell.setAttribute('id', `1-${i}`);
             rows[1].appendChild(cell);
         };
         for (i in gameboard.board[2]) {
             let cell = document.createElement('div');
+            cell.textContent = gameboard.board[2][i];
+
             cell.classList.add('cell');
             cell.setAttribute('id', `2-${i}`);
             rows[2].appendChild(cell);
         };
     };
 
-    return {displayBoard};
+    const refreshBoard = () => {
+        const body = document.querySelector('body');
+        while (body.lastElementChild) {
+            body.removeChild(body.lastElementChild);
+        };
+    };
+
+    return {displayBoard, refreshBoard};
 })();
 
 
 //running stuff
 console.log(gameboard.board);
+displayController.displayBoard();
+
+const cells = document.querySelectorAll('.cell');
+
+cells[0].addEventListener('click', () => {
+    gameboard.board[0][0] = game.getActivePlayer().marker;
+    game.switchTurn();
+    console.log(gameboard.board);
+});
+
+cells[1].addEventListener('click', () => {
+    gameboard.board[0][1] = game.getActivePlayer().marker;
+    game.switchTurn();
+    displayController.refreshBoard();
+    displayController.displayBoard();
+    console.log(gameboard.board);
+}); 
+
+cells[2].addEventListener('click', () => {
+    gameboard.board[0][2] = game.getActivePlayer().marker;
+    game.switchTurn();
+    displayController.refreshBoard();
+    displayController.displayBoard();
+    console.log(gameboard.board);
+}); 
+
+cells[3].addEventListener('click', () => {
+    gameboard.board[1][0] = game.getActivePlayer().marker;
+    game.switchTurn();
+    displayController.refreshBoard();
+    displayController.displayBoard();
+    console.log(gameboard.board);
+}); 
+
+cells[4].addEventListener('click', () => {
+    gameboard.board[1][1] = game.getActivePlayer().marker;
+    game.switchTurn();
+    displayController.refreshBoard();
+    displayController.displayBoard();
+    console.log(gameboard.board);
+}); 
+
+cells[5].addEventListener('click', () => {
+    gameboard.board[1][2] = game.getActivePlayer().marker;
+    game.switchTurn();
+    displayController.refreshBoard();
+    displayController.displayBoard();
+    console.log(gameboard.board);
+}); 
+
+cells[6].addEventListener('click', () => {
+    gameboard.board[2][0] = game.getActivePlayer().marker;
+    game.switchTurn();
+    displayController.refreshBoard();
+    displayController.displayBoard();
+    console.log(gameboard.board);
+}); 
+
+cells[7].addEventListener('click', () => {
+    gameboard.board[2][1] = game.getActivePlayer().marker;
+    game.switchTurn();
+    displayController.refreshBoard();
+    displayController.displayBoard();
+    console.log(gameboard.board);
+}); 
+
+cells[8].addEventListener('click', () => {
+    gameboard.board[2][2] = game.getActivePlayer().marker;
+    game.switchTurn();
+    displayController.refreshBoard();
+    displayController.displayBoard();
+    console.log(gameboard.board);
+}); 
+
